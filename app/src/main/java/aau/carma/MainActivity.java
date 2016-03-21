1e12cc0d3f4e28cb5a4d656c241db72ec7e35672
Kasper Lind Sørensen <omglolkkthxbye@gmail.com>
2016-03-21 14:14:47 +0100

    Adds buttons to main activity for adding a new gesture and recognizing a gesture.

--- app/src/main/java/aau/carma/MainActivity.java
+++ app/src/main/java/aau/carma/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.estimote.sdk.SystemRequirementsChecker;
@@ -50,6 +51,18 @@ public class MainActivity extends AppCompatActivity implements ContextRecognizer
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         gestureRecognizer = new ThreeDOneCentRecognizer(this);
+        findViewById(R.id.add_gesture).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TrainGesture(DEFAULT_LABEL);
+            }
+        });
+        findViewById(R.id.recognize_gesture).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RecognizeGesture();
+            }
+        });
     }
 
     @Override
