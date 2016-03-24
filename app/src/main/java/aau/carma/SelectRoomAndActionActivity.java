5349ccb41dd29317607f3af268b265eeab025162
Kasper Lind Sørensen <omglolkkthxbye@gmail.com>
2016-03-24 01:03:44 +0100

    SelectRoomAndActionActivity now handles the Action model properly.

--- app/src/main/java/aau/carma/SelectRoomAndActionActivity.java
+++ app/src/main/java/aau/carma/SelectRoomAndActionActivity.java
@@ -1,31 +1,38 @@
 package aau.carma;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import aau.carma.Library.Action;
 import aau.carma.Library.ActionsManager;
+import aau.carma.Library.Logger;
 import aau.carma.Library.Optional;
 import aau.carma.Library.Room;
+import aau.carma.Library.RoomsManager;
 
 public class SelectRoomAndActionActivity extends AppCompatActivity {
 
     public static String CONFIGURATION_COUNT = "configurationCount";
+    public static int RESULT_NO_ACTIONS = 2;
 
     private ArrayList<String[]> newConfigurations = new ArrayList<>();
     private String selectedRoom;
-    private String selectedAction;
+    private Action selectedAction;
     private Intent intent;
 
     @Override
@@ -41,18 +48,17 @@ public class SelectRoomAndActionActivity extends AppCompatActivity {
         final ListView actionsListView = (ListView) findViewById(R.id.actions_listView);
         ArrayAdapter<String> roomsAdapter = new ArrayAdapter<String>(this, R.layout.simple_listview_item);
         for (Room room : DummyData.getAllRooms()){
-            roomsAdapter.add(room.name);
+            roomsAdapter.add(room.identifier);
         }
         roomsListView.setAdapter(roomsAdapter);
 
-        ArrayAdapter<String> actionArrayAdapter = new ArrayAdapter<>(this, R.layout.simple_listview_item);
         Optional<ArrayList<Action>> actions = ActionsManager.getInstance().getActions();
-        if (actions.isPresent()) {
-            for (Action action : actions.value) {
-                actionArrayAdapter.add(action.itemLabel + " : " + action.newState);
-            }
-            actionsListView.setAdapter(actionArrayAdapter);
+        if (!actions.isPresent()) {
+            setResult(RESULT_NO_ACTIONS);
+            finish();
         }
+        final ActionArrayAdapter actionArrayAdapter = new ActionArrayAdapter(this, R.layout.simple_listview_item, actions.value);
+        actionsListView.setAdapter(actionArrayAdapter);
 
         roomsListView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
         actionsListView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
@@ -65,7 +71,7 @@ public class SelectRoomAndActionActivity extends AppCompatActivity {
         actionsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                selectedAction = parent.getItemAtPosition(position).toString();
+                selectedAction = actionArrayAdapter.getItem(position);
             }
         });
 
@@ -77,7 +83,7 @@ public class SelectRoomAndActionActivity extends AppCompatActivity {
                 if (selectedAction == null || selectedRoom == null){
                     return;
                 }
-                newConfigurations.add(new String[]{selectedRoom, selectedAction, intent.getStringExtra(SelectGestureActivity.GESTURE_LABEL_EXTRA)});
+                newConfigurations.add(new String[]{selectedRoom, selectedAction.itemName, selectedAction.itemLabel, selectedAction.newState, intent.getStringExtra(SelectGestureActivity.GESTURE_LABEL_EXTRA)});
             }
         });
 
@@ -85,7 +91,7 @@ public class SelectRoomAndActionActivity extends AppCompatActivity {
         doneBindingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (newConfigurations != null || !newConfigurations.isEmpty()){
+                if (newConfigurations == null || newConfigurations.isEmpty()){
                     setResult(RESULT_CANCELED);
                 } else {
                     Bundle bundle = new Bundle();
@@ -100,5 +106,36 @@ public class SelectRoomAndActionActivity extends AppCompatActivity {
                     finish();
             }
         });
+    }
+
+
+    private class ActionArrayAdapter extends ArrayAdapter<Action> {
+
+        private ArrayList<Action> actions = new ArrayList<>();
+        private Context context;
+
+        public ActionArrayAdapter(Context context, int resource, ArrayList<Action> actions) {
+            super(context, resource, actions);
+            this.actions = actions;
+            this.context = context;
+        }
+
+        @Override
+        public Action getItem(int position) {
+            return actions.get(position);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.simple_listview_item, parent, false);
+            }
+
+            TextView actionTextView = (TextView) convertView.findViewById(R.id.gesture_label);
+            Action action = actions.get(position);
+            actionTextView.setText(action.itemLabel + " : " + action.newState);
+            return convertView;
+        }
     }
 }
