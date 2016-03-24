a7033abf8d699ce731a4c26bd6b8ce5df51925cf
Kasper Lind Sørensen <omglolkkthxbye@gmail.com>
2016-03-24 01:01:26 +0100

    Action class now has an identifier.

--- app/src/main/java/aau/carma/Library/Action.java
+++ app/src/main/java/aau/carma/Library/Action.java
@@ -5,6 +5,11 @@ package aau.carma.Library;
  */
 public class Action {
     /**
+     * Database id of the action.
+     */
+    public final String id;
+
+    /**
      * Name of the item the action can be triggered on.
      */
     public final String itemName;
@@ -27,8 +32,21 @@ public class Action {
      * @param newState New state the item should be put in when the action is triggered.
      */
     public Action(String itemName, String itemLabel, String newState) {
+        this(itemName, itemLabel, newState, "");
+    }
+
+    /**
+     * Initializes an action that encapsulates an item using its name
+     * and a new state the item can be put in.
+     * @param itemName Name of the item the action can be triggered on.
+     * @param itemLabel Label of the item the action can be triggered on.
+     * @param newState New state the item should be put in when the action is triggered.
+     * @param id Database id of the action.
+     */
+    public Action(String itemName, String itemLabel, String newState, String id) {
         this.itemName = itemName;
         this.itemLabel = itemLabel;
         this.newState = newState;
+        this.id = id;
     }
 }
