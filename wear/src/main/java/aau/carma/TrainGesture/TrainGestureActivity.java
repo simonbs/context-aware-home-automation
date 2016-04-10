8409465aa7fd46b5057aaaad4448ef336bcb3c4b
Simon Støvring <mail@simonbs.dk>
2016-04-10 16:33:09 +0200

    Adds gesture training

--- wear/src/main/java/aau/carma/TrainGesture/TrainGestureActivity.java
+++ wear/src/main/java/aau/carma/TrainGesture/TrainGestureActivity.java
@@ -1,24 +1,40 @@
 package aau.carma;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
+import aau.carmakit.ThreeDOneCentGestureRecognizer.datatype.ThreeDLabeledStroke;
+import aau.carmakit.ThreeDOneCentGestureRecognizer.datatype.ThreeDPoint;
+import aau.carmakit.ThreeDOneCentGestureRecognizer.recognizer.ThreeDOneCentRecognizer;
 import aau.carmakit.Utilities.Logger;
 
 /**
  * Activity for training a gesture.
  */
-public class TrainGestureActivity extends Activity {
+public class TrainGestureActivity extends Activity implements SensorEventListener {
     /**
      * Intent extra key for specifying the gesture name,
      */
     public final static String IntentExtraKeyGestureName = "gesture_name";
+
+    /**
+     * Minimum amount of samples that must be taken before the gesture can be saved.
+     */
+    private final static int MinimumSampleCount = 1;
 
     /**
      * Name of the gesture to train.
@@ -35,12 +51,53 @@ public class TrainGestureActivity extends Activity {
      */
     private int sampleCount = 0;
 
+    /**
+     * Manages the sensors.
+     */
+    private SensorManager sensorManager;
+
+    /**
+     * Accelerometer sensor. Samples from the sensor is
+     * used in the strokes.
+     */
+    private Sensor accelerometerSensor;
+
+    /**
+     * Stroke currently being recognized.
+     */
+    private ThreeDLabeledStroke tempStroke;
+
+    /**
+     * Recognized strokes ("samples").
+     */
+    private ArrayList<ThreeDLabeledStroke> strokes;
+
+    /**
+     * Gesture recognizer.
+     */
+    private ThreeDOneCentRecognizer recognizer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_train_gesture);
         gestureName = getIntent().getStringExtra(IntentExtraKeyGestureName);
         showSampleCount(sampleCount);
+
+        TextView nameTextView = (TextView)findViewById(R.id.train_gesture_name);
+        nameTextView.setText(gestureName);
+
+        final Button doneButton = (Button)findViewById(R.id.train_gesture_done);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        recognizer = new ThreeDOneCentRecognizer(this);
     }
 
     /**
@@ -58,6 +115,8 @@ public class TrainGestureActivity extends Activity {
     private void incrementSampleCount() {
         sampleCount += 1;
         showSampleCount(sampleCount);
+        Button doneButton = (Button)findViewById(R.id.train_gesture_done);
+        doneButton.setEnabled(sampleCount >= MinimumSampleCount);
     }
 
     /**
@@ -67,13 +126,17 @@ public class TrainGestureActivity extends Activity {
         View contentView = findViewById(android.R.id.content);
         if (isSampling) {
             // Finish this sample.
-            isSampling = false;
+            sensorManager.unregisterListener(this);
+            tempStroke = null;
             contentView.setBackgroundColor(getResources().getColor(R.color.black));
             incrementSampleCount();
+            isSampling = false;
         } else {
             // Start sampling.
             isSampling = true;
             contentView.setBackgroundColor(getResources().getColor(R.color.orange));
+            tempStroke = new ThreeDLabeledStroke(gestureName);
+            sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
         }
     }
 
@@ -85,4 +148,14 @@ public class TrainGestureActivity extends Activity {
 
         return super.dispatchTouchEvent(ev);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (isSampling && tempStroke != null) {
+            tempStroke.addPoint(new ThreeDPoint(event.values[0], event.values[1], event.values[2], event.timestamp));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
 }
