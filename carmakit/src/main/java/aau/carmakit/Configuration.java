38b57456c0709cc032cf44c7b6b3e6de38a591a4
Simon Støvring <mail@simonbs.dk>
2016-04-14 09:43:09 +0200

    Adds comment

--- carmakit/src/main/java/aau/carmakit/Configuration.java
+++ carmakit/src/main/java/aau/carmakit/Configuration.java
@@ -3,13 +3,13 @@ package aau.carmakit;
 import java.util.UUID;
 
 /**
- * Created by simonbs on 08/03/2016.
+ * Global configurations for the app.
  */
 public class Configuration {
     public static final String Log = "CARMAApp";
     public static final Boolean LogEnabled = true;
 
-    public static final String openHABBaseURL = "http://c2083e7f.ngrok.io/rest";
+    public static final String openHABBaseURL = "http://3c1c02a5.ngrok.io/rest";
 
     public static final String EstimoteAppId = "whereami-96k";
     public static final String EstimoteAppToken = "0c545e845da025fe91061318dea910ff";
